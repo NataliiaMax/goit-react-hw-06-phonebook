3ed6a0cf3d64d64c48e53e8727465c33937ce14d
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2023-03-01 11:09:47 +0200

    update filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getFilteredContacts } from 'redux/filter/filter.selectors'; 
-import PropTypes from 'prop-types';
 import style from './ContactList.module.css';
 import ContactUser from '../ContactUser/ContactUser';
 
@@ -19,12 +18,3 @@ export default function ContactList() {
   );
 }
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-};
--- src/redux/filter/filter.selectors.js
+++ src/redux/filter/filter.selectors.js
@@ -1,16 +1,16 @@
-import { getContacts } from "redux/contacts/contactsSelectors";
+import { getContacts } from 'redux/contacts/contactsSelectors';
 
 export const getFilter = state => state.filter;
+
+// console.log(state);
 
 export const getFilteredContacts = state => {
   const filter = getFilter(state);
   const contacts = getContacts(state);
-
   const normalizedFilter = filter.toLowerCase();
-  return (
-    contacts &&
-    contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    )
+
+  console.log(state);
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
   );
 };
