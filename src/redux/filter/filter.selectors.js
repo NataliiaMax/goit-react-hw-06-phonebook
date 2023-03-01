8264bd3dc5ef69e7a845087b895cc70cd60bd28b
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2023-03-01 11:28:01 +0200

    update contacts

--- src/redux/filter/filter.selectors.js
+++ src/redux/filter/filter.selectors.js
@@ -2,11 +2,11 @@ export const getFilter = state => state.filter;
 
 export const getFilteredContacts = state => {
   const filter = state.filter;
-  const contacts = state.contacts;
+  const users = state.contacts;
   const normalizedFilter = filter.toLowerCase();
 
   // console.log(state);
-  return contacts.filter(contact =>
+  return users.filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter)
   );
 };
