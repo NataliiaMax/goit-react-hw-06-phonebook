43d343573c8b042d0ff0db6a476ae86d712468fe
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2023-03-01 11:18:55 +0200

    update filter-file

--- src/redux/filter/filter.selectors.js
+++ src/redux/filter/filter.selectors.js
@@ -1,15 +1,11 @@
-import { getContacts } from 'redux/contacts/contactsSelectors';
-
 export const getFilter = state => state.filter;
 
-// console.log(state);
-
 export const getFilteredContacts = state => {
-  const filter = getFilter(state);
-  const contacts = getContacts(state);
+  const filter = state.filter;
+  const contacts = state.contacts;
   const normalizedFilter = filter.toLowerCase();
 
-  console.log(state);
+  // console.log(state);
   return contacts.filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter)
   );
