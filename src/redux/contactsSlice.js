6113bc07b8aa91f2cbd10e158c7722c91ccdbd97
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2023-03-02 20:55:48 +0200

    update files

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -13,16 +13,16 @@ const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
   reducers: {
-    addContact:(state, action) => {
- 
+    addContact: (state, action) => {
       state.contacts = [action.payload, ...state.contacts];
     },
     deleteContact: (state, action) => {
       state.contacts = state.contacts.filter(
-        contact => contact.id !== action.payload)
-    }
-  }
-})
+        contact => contact.id !== action.payload
+      );
+    },
+  },
+});
 
 export const { addContact, deleteContact } = contactsSlice.actions;
 export default contactsSlice.reducer;
